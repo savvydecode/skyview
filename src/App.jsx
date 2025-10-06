710f2eb27eea6bbff3b889b2e12c71489544380c
savvycode <addostephen557@gmail.com>
2025-10-06 18:45:11 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>SkyView</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+export default function Navbar(){
+    return(<>
+        <LInk to={'/'}>Home</LInk>
+        <LInk to={'/about'}>About</LInk>
+        <LInk to={'/settings'}>Settings</LInk>
+        <LInk to={'/profile'}>Profile</LInk>
+    </>)
+}
